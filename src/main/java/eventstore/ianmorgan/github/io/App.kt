9f0db64f1056303fdbe29b83df996e1cef322aac
Ian Morgan <ian.j.morgan@gmail.com>
2018-05-17 17:05:55 +0100

    remove unused User & UserDao

--- src/main/java/eventstore/ianmorgan/github/io/App.kt
+++ src/main/java/eventstore/ianmorgan/github/io/App.kt
@@ -13,8 +13,6 @@ class JavalinApp(private val port: Int) {
     fun init(): Javalin {
 
 
-        val userDao = UserDao()
-
         val app = Javalin.create().apply {
             port(port)
             //exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
@@ -23,37 +21,7 @@ class JavalinApp(private val port: Int) {
 
         app.routes {
 
-            get("/users") { ctx ->
-                ctx.json(userDao.users)
-            }
 
-            get("/users/:id") { ctx ->
-                ctx.json(userDao.findById(ctx.param("id")!!.toInt())!!)
-            }
-
-            get("/users/email/:email") { ctx ->
-                ctx.json(userDao.findByEmail(ctx.param("email")!!)!!)
-            }
-
-            post("/users/create") { ctx ->
-                val user = ctx.bodyAsClass(User::class.java)
-                userDao.save(name = user.name, email = user.email)
-                ctx.status(201)
-            }
-
-            patch("/users/update/:id") { ctx ->
-                val user = ctx.bodyAsClass(User::class.java)
-                userDao.update(
-                    id = ctx.param("id")!!.toInt(),
-                    user = user
-                )
-                ctx.status(204)
-            }
-
-            delete("/users/delete/:id") { ctx ->
-                userDao.delete(ctx.param("id")!!.toInt())
-                ctx.status(204)
-            }
 
         }
 
--- src/main/java/eventstore/ianmorgan/github/io/User.kt
+++
@@ -1,3 +0,0 @@
-package eventstore.ianmorgan.github.io
-
-data class User(val name: String, val email: String, val id: Int);
